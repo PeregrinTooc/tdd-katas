5770ae593290835b44cc66fbf9daec60645b1b79
Philipp Rembold <Philipp.Rembold@sap.com>
2023-03-03 15:24:11 +0100

    race kata

---
+++ src/stopWatch/RacingTests.java
@@ -0,0 +1,34 @@
+package stopWatch;
+
+import org.junit.jupiter.api.Test;
+
+public class RacingTests {
+    @Test
+    void shouldKnowTheElapsedTime{
+        givenANewRaceWithParticipants();
+        WhenTheRaceIsStarted();
+        AndTimeHasElapsed(0);
+        ThenTheRaceIsNotFinished();
+        AndItsDurationIs(0);
+    }
+
+    private void AndItsDurationIs(int i) {
+
+    }
+
+    private void ThenTheRaceIsNotFinished() {
+
+    }
+
+    private void AndTimeHasElapsed(int i) {
+
+    }
+
+    private void WhenTheRaceIsStarted() {
+
+    }
+
+    private void givenANewRaceWithParticipants() {
+
+    }
+}
